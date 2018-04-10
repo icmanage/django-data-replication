916715ef90dbb46b3167f114c35853b79e8aa137
Ararat Khachatryan <ararat_khachatryan@instigatedesign.com>
2018-04-10 15:33:24 +0400

    Adopted to icm_ipcatalog Django2.0 ported version.

--- data_replication/migrations/0001_initial.py
+++ data_replication/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('object_id', models.PositiveIntegerField(db_index=True)),
                 ('state', models.IntegerField(choices=[(-1, 'Locked'), (0, 'Not Replicated'), (1, 'In Storage'), (2, 'Removed')])),
                 ('last_updated', models.DateTimeField()),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType', on_delete=models.CASCADE)),
             ],
             options={
                 'permissions': (('view_replication', 'View Replication'),),
@@ -31,13 +31,13 @@ class Migration(migrations.Migration):
                 ('replication_type', models.IntegerField(choices=[(1, 'Mongo'), (2, 'Splunk')])),
                 ('state', models.SmallIntegerField(choices=[(1, 'Ready'), (2, 'In-Process')])),
                 ('last_updated', models.DateTimeField()),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType', on_delete=models.CASCADE)),
             ],
         ),
         migrations.AddField(
             model_name='replication',
             name='tracker',
-            field=models.ForeignKey(to='data_replication.ReplicationTracker'),
+            field=models.ForeignKey(to='data_replication.ReplicationTracker', on_delete=models.CASCADE),
         ),
         migrations.AlterUniqueTogether(
             name='replication',
