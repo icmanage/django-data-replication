b9f518c2e247361ea2203f49d18fc80eefeee6cb
Steven Klass <rh0dium@users.noreply.github.com>
2018-04-26 10:34:05 -0600

    Release

--- data_replication/__init__.py
+++ data_replication/__init__.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 
 __name__ = "data_replication"
 __author__ = 'Steven Klass'
-__version_info__ = (1, 0, '0rc2')
+__version_info__ = (1, 0, 0)
 __version__ = '.'.join(map(str, __version_info__))
 __date__ = '9/21/17 07:55'
 __copyright__ = 'Copyright 2017 IC Manage. All rights reserved.'
