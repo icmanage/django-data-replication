dc00205a39dd6fa7551bfc1d70dcd7d97fd66d56
Phoenix Yoder <yoder.phoenix@gmail.com>
2024-05-11 13:44:26 -0700

    Made some skeletons for testing mongo. All the code I try to actually have do something is giving me errors tho.

---
+++ data_replication/tests/test_mongo.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+import objects
+from django.contrib.contenttypes.models import ContentType
+from django.urls import resolve
+from django.utils.timezone import now
+from django import test
+import logging
+from pymongo.errors import ConnectionFailure, OperationFailure
+from data_replication.models import ReplicationTracker
+from data_replication.tasks import push_mongo_objects
+import data_replication.backends.mongo as mongo
+
+
+class MongoTestCase(test.TestCase):
+
+    def test_task_name(self):
+        #ct = ContentType.objects.get_for_model(mongo.MongoRequest)
+        #mtn = ct.all()
+        #self.assertEqual(push_mongo_objects.task_name(), 'push_mongo_objects')
+        pass
+
+    def test_delete_items(self):
+        #ct = ContentType.objects.get_for_model(mongo.MongoRequest)
+        pass
+
+    def test_get_task_kwargs(self):
+        pass
