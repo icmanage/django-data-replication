c66ea6e97b5a8f73830021607dfb5904d78dd5c7
Phoenix Yoder <yoder.phoenix@gmail.com>
2024-07-09 02:19:04 +0000

    Version Bump [skip ci]

--- data_replication/__init__.py
+++ data_replication/__init__.py
@@ -4,8 +4,8 @@
 
 __name__ = "data_replication"
 __author__ = "Steven Klass"
-__version_info__ = (1, 1, 5)
-__version__ = "1.1.5"
+__version_info__ = (2, 0, 0)
+__version__ = "2.0.0"
 __date__ = "9/21/17 07:55"
 __copyright__ = "Copyright 2017 IC Manage. All rights reserved."
 __credits__ = [
